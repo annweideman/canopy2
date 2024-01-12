9ff78d05c22d27f3a36da92cc28282e7cedbfab9
anndo1 <anndo1@umbc.edu>
2024-01-12 15:21:36 -0500

    Further edits to Lag ACF plots

--- R/get_diagnostics.R
+++ R/get_diagnostics.R
@@ -160,8 +160,9 @@ get_diagnostics<-function(get.trees.out, project=NULL, outpath=NULL){
   count<-1
   start<-1
   end<-(Klist-(min(Klist)-1))*nchains
+  if(nchains<=5){n.row=1}else{n.row=2}
   for (K in Klist){
-    graphics::par(mfrow = c(2, 5),oma = c(2, 0, 4, 0))
+    graphics::par(mfrow = c(2, 5),oma = c(2, 0, 6, 0))
     for(i in start:end[count]){
       stats::acf(post.list[[i]],
                  main=paste("Chain", eval(parse(text="chain.id"))))
